b62b442ec773accb50c7f15e4a325e24612eda4b
Maxwell Huang-Hobbs <mahuangh@microsoft.com>
2021-04-21 13:34:20 -0400

    avoid references to arguments in objectSpread
    At present, https://github.com/swc-project/swc/issues/1462 means that
    any project which builds using swc and uses the object-spread helper
    will not object-spread correctly.
    
    This workaround manually binds `arguments` to a new name to sidestep
    that issue.

--- src/_object_spread.js
+++ src/_object_spread.js
@@ -1,14 +1,17 @@
 import defineProperty from './_define_property';
 
 export default function _objectSpread(target) {
-  for (var i = 1; i < arguments.length; i++) {
-    var source = arguments[i] != null ? arguments[i] : {};
+  const vargs = arguments;
+  for (var i = 1; i < vargs.length; i++) {
+    var source = vargs[i] != null ? vargs[i] : {};
     var ownKeys = Object.keys(source);
 
-    if (typeof Object.getOwnPropertySymbols === 'function') {
-      ownKeys = ownKeys.concat(Object.getOwnPropertySymbols(source).filter(function (sym) {
-        return Object.getOwnPropertyDescriptor(source, sym).enumerable;
-      }));
+    if (typeof Object.getOwnPropertySymbols === "function") {
+      ownKeys = ownKeys.concat(
+        Object.getOwnPropertySymbols(source).filter(function (sym) {
+          return Object.getOwnPropertyDescriptor(source, sym).enumerable;
+        })
+      );
     }
 
     ownKeys.forEach(function (key) {
